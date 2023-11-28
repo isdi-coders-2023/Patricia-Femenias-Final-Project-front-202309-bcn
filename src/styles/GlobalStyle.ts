1db55f944b3562dc8afc8d536c1aa77001423a46
Patricia <patricia.femenias@gmail.com>
2023-11-28 20:05:29 +0100

    Add GlobalStyle

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,
+ ::before,
+ ::after {
+   box-sizing: border-box;
+  }
+
+  body,
+  h1, h2 {
+   margin: 0;
+      background: ${({ theme }) => theme.colors.degraded};
+
+  }
+
+  html {
+    font-family:  ${({ theme }) => theme.typography.mainFontFamily};
+    color: ${({ theme }) => theme.colors.main};
+  }
+
+  p {
+    margin: 0;
+  }
+
+  ul, li  {
+   list-style: none;
+   margin: 0;
+   padding-left: 0;
+  }
+
+  img {
+   width: 100%;
+  }
+
+  a {
+   text-decoration: none;
+   color: inherit;
+  }     
+
+  button {
+   border: none;
+   background-color: inherit;
+   font: inherit;
+   cursor: pointer;  
+  }
+
+  input, textarea {
+    font: inherit;
+    border: none
+  }
+`;
+
+export default GlobalStyle;
