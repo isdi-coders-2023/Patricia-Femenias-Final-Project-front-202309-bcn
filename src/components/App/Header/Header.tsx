5d134087e7c4e7d5e8fcda657a66d6efd552930d
Patricia <patricia.femenias@gmail.com>
2023-11-28 21:33:30 +0100

    Add Header component

---
+++ src/components/App/Header/Header.tsx
@@ -0,0 +1,14 @@
+const Header = (): React.ReactElement => {
+  return (
+    <header>
+      <img
+        src="/images/logo.webp"
+        alt="My 90s Furby collection logo"
+        width="300"
+        height="300"
+      />
+    </header>
+  );
+};
+
+export default Header;
