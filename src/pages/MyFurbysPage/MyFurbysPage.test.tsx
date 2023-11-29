3da508883eaa7642d3a280bb2f081c07179b90cc
Patricia <patricia.femenias@gmail.com>
2023-11-29 11:54:04 +0100

    Add MyFurbysPage test

---
+++ src/pages/MyFurbysPage/MyFurbysPage.test.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "../../styles/mainTheme";
+import MyFurbysPage from "./MyFurbysPage";
+
+describe("Given the MyFurbysPage component", () => {
+  describe("When it receives a heading with the title 'My Furby collection'", () => {
+    test("Then it should show a heading with the title 'My Furby collection'", () => {
+      const expectedTitle = "My Furby collection";
+
+      render(
+        <BrowserRouter>
+          <ThemeProvider theme={mainTheme}>
+            <MyFurbysPage />
+          </ThemeProvider>
+        </BrowserRouter>,
+      );
+
+      const heading = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
