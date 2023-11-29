cfe492ae24c0bc5c5a99b2180b5e1bfa5e641acc
Patricia <patricia.femenias@gmail.com>
2023-11-29 19:17:16 +0100

    Add FurbysListStyled

--- src/components/FurbysList/FurbysList.tsx
+++ src/components/FurbysList/FurbysList.tsx
@@ -1,14 +1,15 @@
 import { useAppSelector } from "../../store/hooks";
+import FurbysListStyled from "./FurbyListStyled";
 
 const FurbysList = (): React.ReactElement => {
   const furbysState = useAppSelector((state) => state.furbysState);
 
   return (
-    <ul>
+    <FurbysListStyled>
       {furbysState.furbys.map((furby) => (
         <li key={furby._id}>{furby.name}</li>
       ))}
-    </ul>
+    </FurbysListStyled>
   );
 };
 
