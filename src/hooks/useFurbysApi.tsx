aad5bfc131802efb51d8f3a764e9e9db1c27d66d
Patricia <patricia.femenias@gmail.com>
2023-11-30 19:32:20 +0100

    Add getFurbysApi custom hook

---
+++ src/hooks/useFurbysApi.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { useCallback } from "react";
+import { FurbyStructure } from "../store/features/furbys/types";
+
+const useFurbysApi = () => {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+  const getFurbysApi = useCallback(async () => {
+    const { data: furbys } = await axios.get<FurbyStructure[]>("/furbys");
+    return furbys;
+  }, []);
+
+  return { getFurbysApi };
+};
+
+export default useFurbysApi;
