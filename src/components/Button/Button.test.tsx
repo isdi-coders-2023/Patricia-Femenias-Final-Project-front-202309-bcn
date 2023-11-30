1a26bf15e7aa4d719d6c30a1fd697ce7c736cf6e
Patricia <patricia.femenias@gmail.com>
2023-11-30 18:25:48 +0100

    Add Button test

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,27 @@
+import { screen } from "@testing-library/react";
+import customRender from "../../testsUtils/customRender";
+import Button from "./Button";
+
+describe("Given a Button component", () => {
+  describe("When it receives the text 'Modify'", () => {
+    test("Test it should show the text 'Modify'", () => {
+      const text = "Modify";
+
+      customRender(<Button text={"Modify"} />);
+      const button = screen.getByRole("button", { name: text });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives the text 'Delete'", () => {
+    test("Test it should show the text 'Delete'", () => {
+      const text = "Delete";
+
+      customRender(<Button text={"Delete"} />);
+      const button = screen.getByRole("button", { name: text });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
