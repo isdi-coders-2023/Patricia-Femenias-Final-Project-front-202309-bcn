48cae7232178038fbef78c0216e70f502f8ae642
Patricia <patricia.femenias@gmail.com>
2023-12-01 20:43:34 +0100

    Add showLoading and hideLoading to useFurbysApi

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,15 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import AppStyled from "./AppStyled";
 import Header from "../Header/Header";
 import MyFurbysPage from "../../pages/MyFurbysPage/MyFurbysPage";
+import Loading from "../Loading/Loading";
+import { useAppSelector } from "../../store/hooks";
 
 const App = (): React.ReactElement => {
+  const uiState = useAppSelector((state) => state.uiState);
+
   return (
     <AppStyled>
+      {uiState.isLoading && <Loading />}
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/my-furbys" />} />
