51cb4073f4c822ebedd7c37989398c8c88277e51
Patricia <patricia.femenias@gmail.com>
2023-12-02 16:10:02 +0100

    Add NotFoundPage component

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+const NotFoundPage = (): React.ReactElement => {
+  return (
+    <main>
+      <img
+        src="/images/errorPage.webp"
+        alt="Error 404: page not found"
+        width="300"
+        height="300"
+      />
+    </main>
+  );
+};
+
+export default NotFoundPage;
