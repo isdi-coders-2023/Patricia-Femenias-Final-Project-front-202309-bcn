c55844b907eba89766564a6608b6400938cc82e3
Patricia <patricia.femenias@gmail.com>
2023-12-04 16:32:14 +0100

    Add minireducer deleteFurby test

---
+++ src/store/features/furbys/__tests__/deleteFurbyreducer.test.tsx
@@ -0,0 +1,21 @@
+import furbysApiMock from "../../../../mocks/furbysApiMock";
+import { deleteFurbyActionCreator, furbysReducer } from "../furbysSlice";
+import { FurbyStateStructure } from "../types";
+
+describe("Given a furbySlice with a deleteFurby minireducer", () => {
+  describe("When it receives a list of Furbys and the Peachy's id", () => {
+    test("Then it should return the list of Furbys without Peachy", () => {
+      const initialState: FurbyStateStructure = { furbys: furbysApiMock };
+      const expectedDeletedFurby = "Peachy";
+
+      const currentFurbysState = furbysReducer(
+        initialState,
+        deleteFurbyActionCreator("6564a27d66ed505ce77a67d3"),
+      );
+
+      currentFurbysState.furbys.forEach((furby) => {
+        expect(furby).not.toHaveProperty("name", expectedDeletedFurby);
+      });
+    });
+  });
+});
