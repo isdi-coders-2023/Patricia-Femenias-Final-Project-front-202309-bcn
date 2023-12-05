0d1e8aff527570d849d2819d5f2bef8be5944a01
Patricia <patricia.femenias@gmail.com>
2023-12-05 10:30:02 +0100

    Test button's new responsabilities in FurbyCard

--- src/components/FurbyCard/FurbyCard.test.tsx
+++ src/components/FurbyCard/FurbyCard.test.tsx
@@ -1,7 +1,8 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import furbyApiMock from "../../mocks/FurbyApiMock";
-import { customRender } from "../../testsUtils/customRender";
+import { customRender } from "../../testsUtils/wrappers";
 import FurbyCard from "./FurbyCard";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a FurbyCard component", () => {
   describe("When it receives Peachy's data", () => {
@@ -23,6 +24,45 @@ describe("Given a FurbyCard component", () => {
       const picture = screen.getByAltText(expectedAltText);
 
       expect(picture).toBeInTheDocument();
+    });
+  });
+
+  describe("when it is rendered", () => {
+    test("Then it should show a button with the text 'Modify'", () => {
+      const expectedButtonText = "Modify";
+
+      customRender(<FurbyCard furby={furbyApiMock} />);
+      const buttonText = screen.getByText(expectedButtonText);
+
+      expect(buttonText).toBeInTheDocument();
+    });
+
+    test("Then it should show a button with the text 'Delete'", () => {
+      const expectedButtonText = "Delete";
+
+      customRender(<FurbyCard furby={furbyApiMock} />);
+      const buttonText = screen.getByText(expectedButtonText);
+
+      expect(buttonText).toBeInTheDocument();
+    });
+  });
+
+  describe("When the card with the Furby Peachy is rendered and the user clicks on the button 'Delete'", () => {
+    test("The it should remove the card", async () => {
+      const expectedButtonText = "Delete";
+
+      customRender(<FurbyCard furby={furbyApiMock} />);
+
+      const deleteButton = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+      const furbyName = screen.getByRole("heading", { name: "Peachy" });
+
+      await userEvent.click(deleteButton);
+
+      waitFor(() => {
+        expect(furbyName).not.toBeInTheDocument();
+      });
     });
   });
 });
