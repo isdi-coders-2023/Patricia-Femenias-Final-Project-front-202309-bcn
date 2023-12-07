2f19ea333955800007ab5c38ca0f13c45aa93ff6
Patricia <patricia.femenias@gmail.com>
2023-12-07 17:54:32 +0100

    Add minireducer addNewFurby test

---
+++ src/mocks/newFurbysList.ts
@@ -0,0 +1,41 @@
+import { FurbyStructure } from "../store/features/furbys/types";
+
+const newFurbysList: FurbyStructure[] = [
+  {
+    _id: "6564a27d66ed505ce77a67d3",
+    name: "Peachy",
+    type: "baby",
+    year: 1999,
+    generation: 1,
+    language: "French",
+    price: 25,
+    howFoundIt:
+      "I bought this Furby through Vinted and it arrived very clean and in perfect condition.",
+    imageUrl: "https://i.ibb.co/0FrzcXK/peachy.webp",
+  },
+  {
+    _id: "6564a27d66ed505ce77a67d4",
+    name: "Gizmo",
+    type: "special edition",
+    year: 2000,
+    generation: 5,
+    language: "English",
+    price: 84,
+    howFoundIt:
+      "I bought this I stumbled upon the adorable Furby Gizmo while exploring a quirky vintage store in downtown London. Its bright colors and playful demeanor instantly caught my eye, and I couldn't resist bringing this delightful companion home with me. through Vinted and it arrived very clean and in perfect condition.",
+    imageUrl: "https://i.ibb.co/ThY8Mdp/gizmo.webp.webp",
+  },
+  {
+    _id: "6564a27d66ed6329ce77a67d4",
+    name: "Tiger",
+    type: "normal",
+    year: 1999,
+    generation: 3,
+    language: "French",
+    price: 23,
+    howFoundIt: "I found it in Vinted a lazy sunday at home",
+    imageUrl: "https://i.ibb.co/ThY8Mdp/tiger.webp.webp",
+  },
+];
+
+export default newFurbysList;
